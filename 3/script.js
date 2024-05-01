d3d0f6fcb69fda8614e0cbb611f4410e1df6e400
HenriqueMND <148295689+HenriqueMND@users.noreply.github.com>
2024-05-01 20:19:04 -0300

    Update script.js

--- 3/script.js
+++ 3/script.js
@@ -1,3 +1,7 @@
+let celsius = (prompt("Digite a temperatura em Graus Celsius"));
 
+fahrenheit = (celsius * 9 / 5) + 32;
+
+console.log("A temperatura de Celsius para Fahrenheit é:", fahrenheit.toFixed(2));
 
 
