2255145af9b6232f6bacee7be615463bdb5add10
HenriqueMND <148295689+HenriqueMND@users.noreply.github.com>
2024-05-01 20:18:00 -0300

    Update script.js

--- 2/script.js
+++ 2/script.js
@@ -1,3 +1,8 @@
+let altura = (prompt("Digita a altura do Retângulo:"));
+let base = (prompt("Digite a base do Retângulo"));
 
+const area = base * altura;
+
+console.log("A area do retangulo é:", area.toFixed(2));
 
 
